137ff1aecd066a095509db753c5c097c275f7b45
Kati Rady-Pentek <katiradypentek@gmail.com>
2023-11-16 12:56:44 -0800

    created initial validator class

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import argparse
+import csv
 
-parser = argparse.ArgumentParser()
-parser.add_argument("sample", help="Print out the contents of CLI argument")
-args = parser.parse_args()
-print(args.sample)
+class Validator():
+    def __init__(self, path):
+        self.path = path
+
+    def validate(self):
+        with open(self.path, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+def main():
+    # read csv path from command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv_path", help="Path to the csv file")
+    args = parser.parse_args()
+
+    # create instance of validator 
+    validator = Validator(args.csv_path)
+    validator.validate()
+
+if __name__ == "__main__":
+    main()
