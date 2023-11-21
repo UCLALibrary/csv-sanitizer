daccd0d0ed197c9c1eb87a897050657b8145a703
Kati Rady-Pentek <katiradypentek@gmail.com>
2023-11-21 10:50:40 -0800

    modified init method + linting

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import argparse
 import csv
 
-class Validator():
+
+class Validator:
     def __init__(self, path):
-        self.path = path
+        with open(path, "r", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            self.rows = [row for row in reader]
+            self.fields = reader.fieldnames
 
     def validate(self):
-        with open(self.path, 'r', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
+        pass
+
 
 def main():
     # read csv path from command line arguments
@@ -15,9 +19,10 @@ def main():
     parser.add_argument("csv_path", help="Path to the csv file")
     args = parser.parse_args()
 
-    # create instance of validator 
+    # create instance of validator
     validator = Validator(args.csv_path)
     validator.validate()
+
 
 if __name__ == "__main__":
     main()
